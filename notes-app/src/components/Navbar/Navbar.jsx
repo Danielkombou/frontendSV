e7d2d00bc8083fda4e4d4783ff6571f92797ff5e
Danielkombou <danielkombou7@gmail.com>
2024-06-25 12:18:38 +0100

    Deploying

--- notes-app/src/components/Navbar/Navbar.jsx
+++ notes-app/src/components/Navbar/Navbar.jsx
@@ -2,9 +2,12 @@ import React, { useState } from 'react'
 import ProfileInfo from '../Cards/ProfileInfo'
 import { useNavigate } from 'react-router-dom'
 import SearchBar from '../SearchBar/SearchBar'
+import { CiMenuBurger } from 'react-icons/ci'
+import { IoCloseSharp } from 'react-icons/io5'
 
 function Navbar({ userInfo, onSearchNote, handleClearSearch }) {
   const [searchQuery, setSearchQuery] = useState("");
+  const [hide, setHide] = useState(false)
 
   const navigate = useNavigate();
   const onLogout = () =>{
@@ -23,6 +26,11 @@ function Navbar({ userInfo, onSearchNote, handleClearSearch }) {
     handleClearSearch()
   };
 
+  const handleDisplay = () => {
+    setHide(!hide)
+    console.log(setHide)
+  }
+
   return (
     <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
         <h2 className="text-xl font-medium text-black py-2">Notes</h2>
@@ -35,6 +43,9 @@ function Navbar({ userInfo, onSearchNote, handleClearSearch }) {
         onClearSearch={onClearSearch}
         />
         <ProfileInfo userInfo={userInfo} onLogout={onLogout}/>
+        <CiMenuBurger onClick={handleDisplay} className='md:hidden'/>
+        {/* <IoCloseSharp className='md:hidden'/> */}
+        <div className="fixed bg-red-500 insert-0"></div>
     </div>
   )
 }
